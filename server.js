c52f071a6d1e4a002c823751766aa09f51980237
kimgi <kimgi@ais-info.co.jp>
2024-06-26 14:10:47 +0900

    server

---
+++ server.js
@@ -0,0 +1,102 @@
+// const express = require('express');
+// const app = express();
+
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http, {cors:{origin:"*"}})
+
+// let userInfo = [];
+
+// io.on('connection', (socket) => {
+//   console.log('New client connected:', socket.id);
+
+//   socket.on('login', (nickName) => {
+//     const info = {
+//       nickName:nickName,
+//       id: socket.id
+//     };
+//     userInfo.push(info)
+//     io.emit('msg', {level: "sys", msg:nickName+" 님이 입장하였습니다.", nickName:""});    
+//     });
+
+//   socket.on('sendMessage', ({nickName:nickName, msg:msg}) => {
+//     socket.broadcast.emit('msg', {level:"", msg: msg, nickName:nickName})
+//   });
+
+//   socket.on('disconnect', () => {
+//     const user = userInfo.find(c=>c.id == socket.id)
+//     if(user) {
+//         socket.broadcast.emit('msg', {level:"sys", msg:user.nickName+" 님이 퇴장하였습니다.", nickName:""});
+//     }
+//   });
+// });
+
+// http.listen(process.env.PORT || 5000, ()=>{
+//     console.log("connected!!!")
+// });
+
+
+
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {cors:{origin:"*"}})
+
+let userInfo = [];
+
+io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id);
+
+  socket.on('login', (nickName) => {
+    const info = {
+      nickName:nickName,
+      id: socket.id
+    };
+
+    userInfo.push(info)
+
+    const getCurrentDate = () => {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const day = String(now.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    };
+
+    const currentDate = getCurrentDate();
+    io.emit('msg', {level: "sys", msg:nickName+" 님이 입장하였습니다.", nickName:"", date: currentDate });    
+    });
+
+  socket.on('sendMessage', ({nickName:nickName, msg:msg}) => {
+
+    const getCurrentDate = () => {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const day = String(now.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    };
+
+    const currentDate = getCurrentDate();
+    socket.broadcast.emit('msg', {level:"", msg: msg, nickName:nickName, date: currentDate})
+  });
+
+  socket.on('disconnect', () => {
+    const user = userInfo.find(c=>c.id == socket.id)
+    if(user) {
+      const getCurrentDate = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      }; 
+      const currentDate = getCurrentDate();
+      socket.broadcast.emit('msg', {level:"sys", msg:user.nickName+" 님이 퇴장하였습니다.", nickName:"", date: currentDate});
+    }
+  });
+});
+
+http.listen(process.env.PORT || 5000, ()=>{
+    console.log("connected!!!")
+});
